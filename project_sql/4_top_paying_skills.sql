f5d957f7b08c937ed08dd848d3ee8319a1216bf1
teo-mal <tmalezidis@gmail.com>
2024-08-08 23:44:32 +0300

    Update 4_top_paying_skills.sql

--- project_sql/4_top_paying_skills.sql
+++ project_sql/4_top_paying_skills.sql
@@ -1,5 +1,5 @@
 /*
-Answer: What are the top skills based on salary?
+Question: What are the top skills based on salary?
 - Look at the average salary associated with each skill for Data Analyst positions
 - Focuses on roles with specified salaries, regardless of location
 - Why? It reveals how different skills impact salary levels for Data Analysts and 
